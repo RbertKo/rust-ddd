8c5b34fabcb2ad22e6fd583be691b336aa710d45
RbertKo <myeongsku@gmail.com>
2021-02-02 13:32:29 +0900

    Create Post and Comment structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Post {
+  context: String,
+  comment: Vec<Comment>,
+  author_name: String,
+  author_email: String,
+}
+
+struct Comment {
+  author_name: String,
+  author_email: String,
+  context: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
