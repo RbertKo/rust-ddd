9dd8740c32a2192466fe429efc963e2144641ca3
RbertKo <myeongsku@gmail.com>
2021-02-06 22:36:21 +0900

    Change order of definition and Define implements for Post

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,23 @@ struct Comment {
   context: String,
 }
 
+impl Post {
+  fn add_comment(comment) {
+
+  } 
+}
+
 fn main() {
+  let post: Post = Post {
+    context: String::from("이것은 테스트를 위한 글입니다."),
+    comment: vec![],
+    author_name: String::from("fakeRbertKo"),
+    author_email: String::from("fake_rbertko@gmail.com")
+  }; 
+
   let comment: Comment = Comment {
     author_name: String::from("RbertKo"),
     author_email: String::from("myeongsku@gmail.com"),
     context: String::from("test")
   };
-
-  let post: Post = Post {
-    context: String::from("이것은 테스트를 위한 글입니다."),
-    comment: vec![comment],
-    author_name: String::from("fakeRbertKo"),
-    author_email: String::from("fake_rbertko@gmail.com")
-  }; 
 }
